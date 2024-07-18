efa4ae54e589b7c09897d56f5f69a400e392603b
unknown <martinperez9713@gmail.com>
2024-07-18 10:27:02 -0300

    ItemDetailContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import ItemCount from './components/ItemCount/ItemCount'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 
 function App() {
-
-  const agregarAlCarrito = (count) => {
-    console.log(count)
-  }
 
   return (
     <div>
       <NavBar />
       <ItemListContainer saludo = "Bienvenidos a mi ecommerce" />
-      <ItemCount stock={5} agregarAlCarrito={agregarAlCarrito} />
+      <ItemDetailContainer />
     </div>
   )
 }
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react"
+import obtenerProductos from "../../data/data.js"
+import ItemDetail from "./ItemDetail"
+
+const ItemDetailContainer = () => {
+  const [producto, setProducto] = useState({})
+
+  useEffect( ()=> {
+
+    obtenerProductos()
+      .then((data)=> {
+        const productoEncontrado = data.find( (productoData) => productoData.id === "Frg333")
+        setProducto(productoEncontrado)
+      })
+
+  }, [] )
+
+  return (
+    <ItemDetail producto={producto} />
+  )
+}
+export default ItemDetailContainer
