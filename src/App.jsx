c105231ecee8626df2c7104d87bfbd41c046ef85
unknown <martinperez9713@gmail.com>
2024-07-25 11:53:53 -0300

    clase 10: Eventos y Desafio: Sincronizar ItemCount con ItemDetail

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EjemploContadorEventListener from "./components/Ejemplos/EjemploContadorEventListener";
+import EjemploFormulario from "./components/Ejemplos/EjemploFormulario";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<ItemListContainer saludo="Bienvenidos a mi ecommerce" />} />
         <Route path="/categoria/:idCategoria" element={<ItemListContainer saludo="Bienvenidos a mi ecommerce" />} />
         <Route path="/detalle/:idProducto" element={<ItemDetailContainer />} />
+        <Route path="/ejemplos" element={<EjemploContadorEventListener />} />
+        <Route path="/formulario" element={ <EjemploFormulario /> } />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Ejemplos/EjemploContadorEventListener.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+const EjemploContadorEventListener = () => {
+  const [contador, setContador] = useState(1);
+
+  const handleClickButton = () => {
+    setContador( (prevState) => prevState + 1 )
+  }
+
+  useEffect(()=> {
+    const botonSumar = document.getElementById("botonSumar")
+
+    botonSumar.addEventListener( "click", handleClickButton )
+
+    //esta funcion se ejecuta cuando nuestro componente se desmonta
+    //eliminamos el evento
+    return () => {
+      botonSumar.removeEventListener( "click", handleClickButton )
+    }
+  }, [])
+
+  return (
+    <div>
+      <p>{contador}</p>
+      <button id="botonSumar">+</button>
+    </div>
+  )
+}
+export default EjemploContadorEventListener
